2fb10e7a6ed9fbae9f535e174aa9096bbedd86d7
Andres Ponce <andreseeponce@gmail.com>
2022-10-07 14:01:31 +0000

    startGUI

--- homework/hw1/gui.py
+++ homework/hw1/gui.py
@@ -5,15 +5,19 @@ class GUI():
     def __init__(self, size=(500,500)):
         self.size = size
         self.window = None
+        self.app = QtWidgets.QApplication(sys.argv)
 
     def start_window(self):
         return self._get_window()
 
     def _get_window(self):
         if self.window == None:
-            self.window = QtWidgets.Qwidget()
+            self.window = QtWidgets.QWidget()
             self.window.resize(500, 500)
             
         return self.window
 
-
+gui = GUI()
+gui.start_window()
+gui.window.show()
+sys.exit(gui.app.exec_())
