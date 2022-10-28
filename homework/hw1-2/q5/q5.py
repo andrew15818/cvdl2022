7eaa4d6a1949e08663ceb25f33eda2928fd796fc
Andres Ponce <andreseeponce@gmail.com>
2022-10-28 13:00:50 +0000

    moved to own folder

---
+++ homework/hw1-2/q5/q5.py
@@ -0,0 +1,11 @@
+import torch
+import matplotlib.pyplot as plt
+
+class ModelInterface():
+    # Set the path for CIFAR-10
+    def __init__(self):
+        pass
+    def show_image_grid():
+        pass
+    def _train(self):
+        pass
